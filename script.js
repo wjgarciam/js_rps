30182a20c4b1cec76affeef935be61c6d81ff17b
william <122491670+wjgarciam@users.noreply.github.com>
2024-02-26 04:23:24 -0800

    Add the correct logic for the computer play choice

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const computerSelection = getComputerChoice(1,3)
 function getComputerChoice(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-
+    let numberPick = Math.floor(Math.random() * (max - min + 1)) + min
+    if (numberPick == 1) {
+        return 'rock'
+    } else if (numberPick == 2) {
+        return 'paper'
+    } else (numberPick == 3); {
+        return 'scissors'
+    };
 }
 
 
-
+console.log(computerSelection)
