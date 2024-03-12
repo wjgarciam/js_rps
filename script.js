eac06b58dc4c3a037e2e4d346e5e530d765ddef7
william <122491670+wjgarciam@users.noreply.github.com>
2024-03-12 04:20:02 -0700

    Add variables for player and computer score

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const computerSelection = getComputerChoice(1,3)
 const playerSelection = getPlayerSelection()
+const matchResult = playRound (playerSelection, computerSelection)
+let playerScore = 0
+let computerScore = 0
 
 function getComputerChoice(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     let numberPick = Math.floor(Math.random() * (max - min + 1)) + min
     if (numberPick == 1) {
-        return 'rock'
+        return "rock"
     } else if (numberPick == 2) {
-        return 'paper'
+        return "paper"
     } else (numberPick == 3); {
-        return 'scissors'
+        return "scissors"
     };
 }
 
 function getPlayerSelection () {
     let playerInput = prompt("Start the game by selecting Rock, Paper, or Scissors", "You must make a valid choice!")
     if (playerInput === null || playerInput === " ") {
-        alert('Please enter an answer.');
+        alert("Please enter a valid choice.");
     return;  
     }
     switch (playerInput.toLowerCase()) {
@@ -35,14 +38,21 @@ function getPlayerSelection () {
 
 function playRound (playerSelection, computerSelection) {
 
-    if (playerSelection == computerSelection) {
-        return +0
-    } else if (playerSelection > computerSelection) {
-        return +1
-    } else if (playerSelection < computerSelection) {
-        return -1
-    } else (playerSelection == null);
-        alert("Don't try me")
+        if (playerSelection == computerSelection) {
+            return 0
+        } else if (playerSelection > computerSelection) {
+            return +1
+        } else if (playerSelection < computerSelection) {
+            return -1
+        } else (playerSelection == null);
+            alert("Don't try me")
+        
+    }
+
+function playGame() {
+
+
+
 }
 
-console.log(playRound (playerSelection, computerSelection))
+console.log(playRound(playerSelection, computerSelection));
